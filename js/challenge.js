e5aaf1619257df9063fe7eec94d049495547d543
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-06-29 16:27:54 -0400

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,15 @@
+const counter = document.getElementById("counter");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const redHeart = document.getElementById("heart");
+const pauseButton = document.getElementById("pause");
+const commentForm = document.getElementById("comment-form"); 
+
+document.addEvenetListener("DOMContentLoaded", () => {
+    setInterval(()=>{ 
+        
+
+    },1000)
+  
+
+}) 
